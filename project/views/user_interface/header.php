65d9301d287904564db11f4d2613c12615cfc254
root <admin@realitygroup.ru>
2011-12-02 12:50:06 +0300

    styles update

--- project/views/user_interface/header.php
+++ project/views/user_interface/header.php
@@ -1,3 +1,14 @@
+<?php if($admin):?>
+	<div class="admin-panel">
+		<div class="container_12 clearfix">
+			<div class="grid_12">
+				<?=anchor('feedback','Отзывы');?>
+				<?=anchor('profile','Сменить пароль');?>
+				<?=anchor('logoff','Завершить сеанс');?>
+			</div>
+		</div>
+	</div>
+<?php endif;?>
 <header>
 	<div class="container_12 clearfix">
 		<div class="grid_6">
@@ -6,13 +17,4 @@
 		<div class="grid_3 top-phone">8 (499) 703-00-78<span>Москва и Московская область</span></div>
 		<div class="grid_3 top-phone last">8 10 34 678-283-024<span>Тенерифе (Канарские острова)</span></div>
 	</div>
-	<?php if($admin):?>
-		<div style="position:absolute; top:75px;left:90%;">
-			<span><?=anchor('feedback','Отзывы');?></span>
-			<br/>
-			<span><?=anchor('profile','Сменить пароль');?></span>
-			<br/>
-			<span><?=anchor('logoff','Завершить сеанс');?></span>
-		</div>
-		<?php endif;?>
 </header>
