8dcf46f215268d3f90bc223f38fc1ecf45e04adb
root <admin@realitygroup.ru>
2011-12-19 13:04:33 +0300

    changed phone, navigation, contacts

--- project/views/user_interface/navigation.php
+++ project/views/user_interface/navigation.php
@@ -1,20 +1,22 @@
 <nav>
 	<div class="container_12 clearfix">
-		<div class="grid_5 suffix_2">
-		  <ul class="main-nav left">
-		  	<li><?=anchor('about','О Тенерифе', 'id="ref-about"');?></li>
-		  	<li><?=anchor('retail','Продажа', 'id="ref-sell"');?></li>
+		<div class="grid_6">
+			<ul class="main-nav left">
+				<li><?=anchor('about','О Тенерифе', 'id="ref-about"');?></li>
+				<li><?=anchor('retail','Продажа', 'id="ref-sell"');?></li>
 				<li><?=anchor('rent/retail','Аренда', 'id="ref-rent"');?></li>
-				<li><?=anchor('tour','Экскурсии', 'id="ref-excursion"');?></li>
-	  	</ul>
+				<li><?=anchor('rent/auto','Прокат авто', 'id="ref-rent-auto"');?></li>
+				<li class="last"><?=anchor('tour','Экскурсии', 'id="ref-excursion"');?></li>
+			</ul>
 		</div>
-		<div class="grid_5">
-	  	<ul class="main-nav right">
-				<li><?=anchor('blog','Блог', 'id="ref-blog" target="_blank"');?></li>
+		<div class="grid_6">
+			<ul class="main-nav right">
+				<li class="last"><?=anchor('blog','Блог', 'id="ref-blog" target="_blank"');?></li>
 				<li><?=anchor('contacts','Контакты', 'id="ref-contacts"');?></li>
 				<li><?=anchor('transfers','Трансферы', 'id="ref-transfers"');?></li>
 				<li><?=anchor('service','Услуги', 'id="ref-services"');?></li>
-	  	</ul>
+				<!--li><?=anchor('service','Авиабилеты', 'id="ref-avia"');?></li-->
+			</ul>
 		</div>
 	</div>
 </nav>
