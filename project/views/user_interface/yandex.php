2f19e10c503a448f979d179f0f185f6a7165c6c7
root <admin@realitygroup.ru>
2011-12-27 22:27:59 +0300

    feedback edited, css, counters

--- project/views/user_interface/yandex.php
+++ project/views/user_interface/yandex.php
@@ -1,10 +1,17 @@
-<div style="display:none;">
-	<script type="text/javascript">
-		(function(w, c){(w[c] = w[c] || []).push(function(){try{w.yaCounter9235579 = new Ya.Metrika({id:9235579, enableAll: true, trackHash:true});}catch(e){}});})(window, "yandex_metrika_callbacks");
-	</script>
-</div>
+<!-- Yandex.Metrika counter -->
+<div style="display:none;"><script type="text/javascript">
+(function(w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter9235579 = new Ya.Metrika({id:9235579, enableAll: true, trackHash:true, webvisor:true});
+        }
+        catch(e) { }
+    });
+})(window, "yandex_metrika_callbacks");
+</script></div>
 <script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript" defer="defer"></script>
 <noscript><div><img src="//mc.yandex.ru/watch/9235579" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 <script type="text/javascript">
 	var _gaq=[['_setAccount','UA-19030454-1'],['_trackPageview'],['_trackPageLoadTime'],['_setAllowLinker', true]];
 	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
